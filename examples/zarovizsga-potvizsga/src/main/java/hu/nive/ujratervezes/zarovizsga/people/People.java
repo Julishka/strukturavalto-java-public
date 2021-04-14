1e01784cc803904fc41d04765d62d9d7b80493bd
unknown <julishka@naver.com>
2021-04-14 14:54:29 +0200

    people implemented

--- examples/zarovizsga-potvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
+++ examples/zarovizsga-potvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
@@ -1,8 +1,28 @@
 package hu.nive.ujratervezes.zarovizsga.people;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
 public class People {
     public int getNumberOfMales(String path) {
-        //TODO
-        return 0;
+        Objects.requireNonNull(path);
+        Path filePath = Paths.get(path);
+        int males = 0;
+        try {
+            List<String> people = Files.readAllLines(filePath);
+            for (String person : people) {
+                String[] split = person.split(",");
+                if (split[4].equals("Male")) {
+                    males++;
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File cannot be found on path " + path);
+        }
+        return males;
     }
 }
