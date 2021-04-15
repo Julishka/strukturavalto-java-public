fd35272d836070e01819a8538f42cd6ad416cff6
unknown <julishka@naver.com>
2021-04-15 15:22:53 +0200

    count chars implemented

--- examples/zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/countchars/DaVinciCode.java
+++ examples/zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/countchars/DaVinciCode.java
@@ -1,8 +1,38 @@
 package hu.nive.ujratervezes.zarovizsga.countchars;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
 public class DaVinciCode {
     public int encode(String path, char character) {
-        //TODO
-        return 0;
+        Objects.requireNonNull(path);
+        if (path.equals("")) {
+            throw new IllegalArgumentException("Path cannot be empty");
+        }
+        if (character != '0' && character != '1' && character != 'x') {
+            throw new IllegalArgumentException("Character shall be either 0, 1 or x");
+        }
+        Path filePath = Paths.get(path);
+        int frequency = 0;
+        try {
+            List<String> lines = Files.readAllLines(filePath);
+            for (String line : lines) {
+                char[] chars = line.toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    if (chars[i] == character) {
+                        frequency++;
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("File cannot be found on path " + path);
+        }
+        return frequency;
     }
 }
