908d6c472e98d2ce3e9468be0d0f51428aca23f9
unknown <julishka@naver.com>
2021-04-15 12:10:10 +0200

    query with multiple parameters implemented

--- examples/zarovizsga-potvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
+++ examples/zarovizsga-potvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
@@ -1,13 +1,22 @@
 package hu.nive.ujratervezes.zarovizsga.peoplesql;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 
 public class PeopleDao {
+    JdbcTemplate jdbcTemplate;
+    DataSource dataSource;
+
     public PeopleDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        this.dataSource = dataSource;
     }
 
     public String findIpByName(String firstName, String lastName) {
-        //TODO
-        return null;
+        Object[] parameters = {firstName, lastName};
+        String sql = "SELECT ip_address FROM people WHERE first_name = ? AND last_name = ?";
+
+        return jdbcTemplate.query(sql, (resultSet, i) -> resultSet.getString("ip_address"), parameters).get(0);
     }
 }
