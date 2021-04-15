0ab7bdc05a03b51db359a19d9bdc5ba416c03f1b
unknown <julishka@naver.com>
2021-04-15 15:12:27 +0200

    unique char implemented

--- examples/zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
+++ examples/zarovizsga-potpotvizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
@@ -1,10 +1,22 @@
 package hu.nive.ujratervezes.zarovizsga.uniquefinder;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class UniqueFinder {
     public List<Character> uniqueChars(String string) {
-        //TODO
-        return null;
+        if (string == null) {
+            throw new IllegalArgumentException("String cannot be null");
+        }
+
+        char[] charArray = string.toCharArray();
+        List<Character> characters = new ArrayList<>();
+        for (char character : charArray) {
+            if (!characters.contains(character)) {
+                characters.add(character);
+            }
+        }
+        return characters;
     }
 }
