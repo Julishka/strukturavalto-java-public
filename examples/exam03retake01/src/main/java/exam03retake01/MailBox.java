8d6d5f46506e4c08b7bca40b412be63cf62d11fd
unknown <julishka@naver.com>
2021-04-21 17:06:30 +0200

    MailBox task implemented

--- examples/exam03retake01/src/main/java/exam03retake01/MailBox.java
+++ examples/exam03retake01/src/main/java/exam03retake01/MailBox.java
@@ -2,6 +2,7 @@ package exam03retake01;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MailBox {
     protected List<Mail> mails;
@@ -15,8 +16,28 @@ public class MailBox {
     }
 
     public List<Mail> findByCriteria(String criteria) {
-        //TODO
-        return null;
+        //TODO: find prettier solution
+        Objects.requireNonNull(criteria);
+        List<Mail> searchResult = new ArrayList<>();
+        for (Mail mail : mails) {
+            if (criteria.startsWith("from:")) {
+                String cropped = criteria.replace("from:", "");
+                if (mail.getFrom().getEmail().equals(cropped) || mail.getFrom().getName().equals(cropped)) {
+                    searchResult.add(mail);
+                }
+            } else if (criteria.startsWith("to:")) {
+                String cropped = criteria.replace("to:", "");
+                for (Contact contact : mail.getTo()) {
+                    if (contact.getName().equals(cropped) || contact.getEmail().equals(cropped)) {
+                        searchResult.add(mail);
+                    }
+                }
+            } else if (mail.getMessage().contains(criteria) || mail.getSubject().contains(criteria)) {
+                searchResult.add(mail);
+            }
+        }
+
+        return searchResult;
     }
 
     public void addMail(Mail mail) {
