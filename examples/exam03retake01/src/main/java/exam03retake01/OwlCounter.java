a40ee54244002790b94d528ca04d28cdb6710014
Julishka <64486433+Julishka@users.noreply.github.com>
2021-04-22 19:38:35 +0200

    owl counter implemented

--- examples/exam03retake01/src/main/java/exam03retake01/OwlCounter.java
+++ examples/exam03retake01/src/main/java/exam03retake01/OwlCounter.java
@@ -1,14 +1,34 @@
 package exam03retake01;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
 
 public class OwlCounter {
-    public void readFromFile(BufferedReader reader) {
-        //TODO
+    protected Map<String, Integer> owlsByCounty;
+
+    public OwlCounter() {
+        this.owlsByCounty = new HashMap<>();
     }
 
-    public int getNumberOfOwls(String county) {
-        //TODO
-        return 0;
+    public Map<String, Integer> readFromFile(BufferedReader reader) {
+        Objects.requireNonNull(reader);
+
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split("=");
+                owlsByCounty.put(split[0], Integer.parseInt(split[1]));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not read file", e);
+        }
+
+        return owlsByCounty;
+    }
+
+    public Integer getNumberOfOwls(String county) {
+        return owlsByCounty.get(county);
     }
 }
